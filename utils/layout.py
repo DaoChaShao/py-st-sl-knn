5d4daf899f749c29c259e38ce9f6ce568aad0b90
DaoChaShao <27882272@qq.com>
2025-09-03 15:49:25 +0800

    feat: add layout.py for page configuration and sidebar navigation

---
+++ utils/layout.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/9/3 15:34
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   layout.py
+# @Desc     :   
+
+from streamlit import set_page_config, Page, navigation
+
+
+def page_config() -> None:
+    """ Set the window
+    :return: None
+    """
+    set_page_config(
+        page_title="Supervised Learning",
+        page_icon=":material/globe:",
+        layout="wide",
+        initial_sidebar_state="expanded",
+    )
+
+
+def pages_setter() -> None:
+    """ Set the subpages on the sidebar
+    :return: None
+    """
+    pages: dict = {
+        "page": [
+            "subpages/home.py",
+            "subpages/preparation.py",
+            "subpages/about.py",
+        ],
+        "title": [
+            "Home",
+            "Data Preparation",
+            "About",
+        ],
+        "icon": [
+            ":material/home:",
+            ":material/dataset:",
+            ":material/info:",
+        ],
+    }
+
+    structure: dict = {
+        "Introduction": [
+            Page(page=pages["page"][0], title=pages["title"][0], icon=pages["icon"][0]),
+        ],
+        "Core Functions": [
+            Page(page=pages["page"][1], title=pages["title"][1], icon=pages["icon"][1]),
+        ],
+        "Information": [
+            Page(page=pages["page"][2], title=pages["title"][2], icon=pages["icon"][2]),
+        ],
+    }
+    pg = navigation(structure, position="sidebar", expanded=True)
+    pg.run()
