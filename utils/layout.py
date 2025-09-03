fa30192b8f7df0bb5c680cf4c0025d0b3860c3de
DaoChaShao <27882272@qq.com>
2025-09-03 16:24:33 +0800

    feat: add model training page to navigation

--- utils/layout.py
+++ utils/layout.py
@@ -29,16 +29,19 @@ def pages_setter() -> None:
         "page": [
             "subpages/home.py",
             "subpages/preparation.py",
+            "subpages/train.py",
             "subpages/about.py",
         ],
         "title": [
             "Home",
             "Data Preparation",
+            "Model Training",
             "About",
         ],
         "icon": [
             ":material/home:",
             ":material/dataset:",
+            ":material/function:",
             ":material/info:",
         ],
     }
@@ -49,9 +52,10 @@ def pages_setter() -> None:
         ],
         "Core Functions": [
             Page(page=pages["page"][1], title=pages["title"][1], icon=pages["icon"][1]),
+            Page(page=pages["page"][2], title=pages["title"][2], icon=pages["icon"][2]),
         ],
         "Information": [
-            Page(page=pages["page"][2], title=pages["title"][2], icon=pages["icon"][2]),
+            Page(page=pages["page"][3], title=pages["title"][3], icon=pages["icon"][3]),
         ],
     }
     pg = navigation(structure, position="sidebar", expanded=True)
