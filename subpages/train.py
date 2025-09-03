7e31f0be3da7cfc955ca53ed0618518ca3a9d541
DaoChaShao <27882272@qq.com>
2025-09-03 16:24:40 +0800

    feat: add initial train.py script with metadata

---
+++ subpages/train.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/9/3 16:23
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   train.py
+# @Desc     :   
+
